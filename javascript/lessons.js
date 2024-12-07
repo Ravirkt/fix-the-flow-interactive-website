2076054f7d1a6338434555d28bc36c075d4a8112
Ravirkt <r_tjikhoeri@live.com>
2024-12-07 17:42:03 +0100

    added form and close /open function js #5

--- javascript/lessons.js
+++ javascript/lessons.js
@@ -1,54 +1,32 @@
 // hier wordt de button geselecteerd aan de hand van de id die gekoppeld is aan de button,
 // vervolgens heb ik een eventlistiners gebruikt dat kijkt of er op de knop wordt gedrukt
-document.getElementById('knop').addEventListener('click', function() {
+// document.getElementById('knop').addEventListener('click', function() {
   // hier heb ik een variabele makenewcard waarin ik ervoor zorg dat het met createlement een html section aanmaakt bij elke klik.
-      let makeNewCard = document.createElement('section');
+      // let makeNewCard = document.createElement('section');
       // er wordt een class genaamd playlist-cards-section toegoevgt aan de section
-      makeNewCard.classList.add('playlist-card-section');  // Voeg de juiste class toe
+      // makeNewCard.classList.add('playlist-card-section');
   
       // met innerhtml zorg ik ervoor dat de gemaakte section deze html bevat.
-      makeNewCard.innerHTML = `
-              <figure class="card-image-figure">
-                  <img class="playlist-image-cover" src="./assets/coverart-4.png" alt="Cover Image">
-              </figure>
-              <h3 class="title-playlist-card-section">Storytime adventures of the friendly turtle</h3>
-              <a href="" class="playlist-play-button" aria-label="Play audio the Pig and cow enjoy their day">
-                  <span class="play-icon" aria-hidden="true">▶</span>
-                  <span class="play-time" aria-hidden="true">93 min. 23 sec</span>
-              </a>
-      `;
+      // makeNewCard.innerHTML = `
+      //         <figure class="card-image-figure">
+      //             <img class="playlist-image-cover" src="./assets/coverart-4.png" alt="Cover Image">
+      //         </figure>
+      //         <h3 class="title-playlist-card-section">Storytime adventures of the friendly turtle</h3>
+      //         <a href="" class="playlist-play-button" aria-label="Play audio the Pig and cow enjoy their day">
+      //             <span class="play-icon" aria-hidden="true">▶</span>
+      //             <span class="play-time" aria-hidden="true">93 min. 23 sec</span>
+      //         </a>
+      // `;
       
       // in mijn html code heb ik een section met de id sectioncontainer met appenchild zorg ik ervoor dat de gemaakte section makenewcard
-      // als een child wordt toegevoegt aan die html section. dus 
-      document.getElementById('sectionsContainer').appendChild(makeNewCard);
+      // als een child wordt toegevoegt aan die html section.
+      // document.getElementById('sectionsContainer').appendChild(makeNewCard);
   
-  });
+  // });
   
 
 
 
-
-
-// document.getElementById('addSectionButton').addEventListener('click', function() {
-//     let newSection = document.createElement('section');
-
-//     newSection.classList.add('new-section');
-
-//     newSection.innerHTML = `
-//     <section class="playlist-card-section">
-//         <figure class="card-image-figure">
-//             <img class="playlist-image-cover" src="../assets/coverart-4.png" alt="Cover Image">
-//         </figure>
-//         <h3 class="title-playlist-card-section">Storytime adventures of the friendly turtle</h3>
-//         <a href="" class="playlist-play-button" aria-label="Play audio the Pig and cow enjoy their day">
-//             <span class="play-icon" aria-hidden="true">▶</span>
-//             <span class="play-time" aria-hidden="true">93 min. 23 sec</span>
-//         </a>
-//     </section>
-// `;
-
-//     document.getElementById('sectionsContainer').appendChild(newSection);
-//   });
 
 
 
@@ -66,3 +44,13 @@ document.getElementById("scroll-button").addEventListener("click", function() {
     });
   });
 
+  
+  document.getElementById("open-dropdown-button").addEventListener("click", function() {
+    const dropDownMenu = document.getElementById("form-popup");
+    dropDownMenu.classList.remove("close-open-form"); 
+});
+
+document.getElementById("close-dropdown-button").addEventListener("click", function() {
+    const dropDownMenu = document.getElementById("form-popup");
+    dropDownMenu.classList.add("close-open-form");   
+});
--- javascript/test.js
+++ javascript/test.js
@@ -1,27 +1,33 @@
 // hier wordt de button geselecteerd aan de hand van de id die gekoppeld is aan de button,
 // vervolgens heb ik een eventlistiners gebruikt dat kijkt of er op de knop wordt gedrukt
-document.getElementById('knop').addEventListener('click', function() {
+// document.getElementById('knop').addEventListener('click', function() {
 // hier heb ik een variabele makenewcard waarin ik ervoor zorg dat het met createlement een html section aanmaakt bij elke klik.
-    let makeNewCard = document.createElement('section');
+    // let makeNewCard = document.createElement('section');
     // er wordt een class genaamd playlist-cards-section toegoevgt aan de section
-    makeNewCard.classList.add('playlist-card-section');  // Voeg de juiste class toe
+    // makeNewCard.classList.add('playlist-card-section'); 
 
     // met innerhtml zorg ik ervoor dat de gemaakte section deze html bevat.
-    makeNewCard.innerHTML = `
-            <figure class="card-image-figure">
-                <img class="playlist-image-cover" src="./assets/coverart-4.png" alt="Cover Image">
-            </figure>
-            <h3 class="title-playlist-card-section">Storytime adventures of the friendly turtle</h3>
-            <a href="" class="playlist-play-button" aria-label="Play audio the Pig and cow enjoy their day">
-                <span class="play-icon" aria-hidden="true">▶</span>
-                <span class="play-time" aria-hidden="true">93 min. 23 sec</span>
-            </a>
-    `;
+    // makeNewCard.innerHTML = `
+    //         <figure class="card-image-figure">
+    //             <img class="playlist-image-cover" src="./assets/coverart-4.png" alt="Cover Image">
+    //         </figure>
+    //         <h3 class="title-playlist-card-section">Storytime adventures of the friendly turtle</h3>
+    //         <a href="" class="playlist-play-button" aria-label="Play audio the Pig and cow enjoy their day">
+    //             <span class="play-icon" aria-hidden="true">▶</span>
+    //             <span class="play-time" aria-hidden="true">93 min. 23 sec</span>
+    //         </a>
+    // `;
     
     // in mijn html code heb ik een section met de id sectioncontainer met appenchild zorg ik ervoor dat de gemaakte section makenewcard
     // als een child wordt toegevoegt aan die html section. dus 
-    document.getElementById('sectionsContainer').appendChild(makeNewCard);
+//     document.getElementById('sectionsContainer').appendChild(makeNewCard);
 
-});
+// });
 
 
+// meer beschrijving bij de bovenste buttons
+// een korte beschrijving bij own playlist
+// een korte beschrijving bij all stories
+// een korte beschrijving onder de titel
+// datum bij de cards wanneer welke playlist toegevoegt is.
+// meest recente card op begin zodat scrollknop weg kan
